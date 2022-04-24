0480d60994d6a0c5d338d5d61fae0905344ee13b
Lovro Habjan <lh5107@student.uni-lj.si>
2022-04-24 16:45:51 +0200

    Potrjevanje zgodbe, product backlog z nav pills
    Potrjevanje zgodbe - backend implementiran v api/ modulu. Za zgodbo,
    ki je v trenutnem zgodbu, je dodan gumb za potrditev zgodbe, ki je
    aktiven le, ko so končane vse naloge.
    
    Backend preveri podatke preden naredi spremembo.
    
    Product backlog vsebuje nedokončane in dokončane zgodbe v obliki
    pillsov.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .stories import StoriesApi
+from .stories import StoriesApi, StoriesConfirmApi
 
 urlpatterns = [
     path('projects/<int:project_id>/stories/<int:story_id>/', StoriesApi.as_view()),
-    path('projects/<int:project_id>/stories/', StoriesApi.as_view())
+    path('projects/<int:project_id>/stories/', StoriesApi.as_view()),
+    path('projects/<int:project_id>/stories/<int:story_id>/confirm', StoriesConfirmApi.as_view())
 ]
